27a1bea076703b230c41831f3c9f008ee69ce273
SharleneRyan <sa.ryan@live.com>
2019-04-20 19:11:27 +1200

    adding Titles.js

---
+++ Data/Titles.js
@@ -0,0 +1,87 @@
+module.exports = {
+  mainTitle,
+  MaoriTitle,
+  GoveTitle,
+  youthTitle,
+  famTitle
+}
+
+
+
+
+
+var mainTitle =[
+  {
+    
+    title: 'Maori Services'
+  },
+  {
+    title: 'Government Services'
+  },
+  {
+    title: 'Youth Services'
+  },
+  {
+    title: 'Services'
+  }
+]
+
+var MaoriTitle =[{
+    
+  title:'Maori Mental Health'
+},
+{
+  title:'Kaumatua'
+},
+{
+  title:'Local Marea'
+},
+{
+  title:''
+}
+]
+
+var GoveTitle = [
+  {
+  title:''
+},
+{
+  title:''
+},
+{
+  title:''
+},
+{
+  title:''
+}
+]
+
+var youthTitle = [
+  {
+    title:''
+  },
+  {
+    title:''
+  },
+  {
+    title:''
+  },
+  {
+    title:''
+}
+]
+
+var famTitle = [
+  {
+    title:''
+  },
+  {
+    title:''
+  },
+  {
+    title:''
+  },
+  {
+    title:''
+  }
+]
