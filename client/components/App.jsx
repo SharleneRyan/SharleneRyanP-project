8e94828448f7c53fc6d38524e2998b3b74015102
SharleneRyan <sa.ryan@live.com>
2019-04-24 20:07:30 +1200

    save

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,65 +1,59 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Grid from './Grid'
-import Footer from './Footer' 
-import Buttons from './Button'
-import Zoom from 'react-reveal/Zoom'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Grid from "./Grid";
+import Footer from "./Footer";
+import Zoom from "react-reveal/Zoom";
+import Contact from './Contact'
+import mainTitle from "../../data/Titles";
+import { catchClause } from "@babel/types";
 
-const App = () => {
-  return (
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "Community Services App"
+        };
+    }
 
-    <div>
+    settitle = newtitle => {
+        console.log("newtitle", newtitle);
+               if (newtitle !== this.state.title){
+         this.setState({ title: newtitle });
+        }
+    };
 
-      
-    <div className="header">
-    <header>
-      <img src="https://www.police.govt.nz/sites/all/themes/twbs_police/logo.png" alt=""/>
-      <h1>
-      <p></p>
-      <Zoom>COMMUNITY SERVICE APP</Zoom>
-      <p></p>
-      <br/>
-      </h1>
-      <img src="" alt=""/>
-      </header>
-    </div>
-    
-    <p></p>
-    <Grid/>
-    <p></p>
-    {/* <Buttons/> */}
-    <p></p>
-    {/* <Footer/> */}
-    </div>
-  )
-  
+    render() {
+        return (
+            <div>
+                <div className="app">
+                    <div className="header">
+                        <header>
+                            <img
+                                src="https://www.police.govt.nz/sites/all/themes/twbs_police/logo.png"
+                                alt=""
+                            />
+                            <h1>
+                                <Zoom>
+                                    <p />
+                                    <div className="Title">
+                                        <h1>{this.state.title}</h1>
+                                    </div>
+                                    <p />
+                                </Zoom>
+                                <br />
+                            </h1>
+                        </header>
+                    </div>
+
+                    <p />
+                    <Grid settitle={this.settitle} />
+                    <p />
+                      <Contact />
+                    <Footer />
+                </div>
+            </div>
+        );
+    }
 }
-  
-// render() {
-//     return (
-//       <div>
-//       <h1>COMMUNITY SERVICE APP</h1>
-//       <ul>
-//         {this.state.FamilyServer.map((FamilyServer,index) => <li key={index}> {index}. {this.Capitalize(`${FamilyServer.name}`)}</li>)}
-//         <li></li>
-//         <li></li>
-//       </ul>
-//       </div>
-//     )
-//   }
 
-
-// class App extends React.Component {
-//   constructor(props){
-//     super(props)
-
-//     this.state={
-//       FamilyService:[{}]
-//     }
-  
-//   }
-// }
-
-
-export default App
-
+export default App;
